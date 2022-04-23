20ff7ee6a5bc6f8a6dcee703e478645c2b084101
Myungchul.Lee <mgcllee@gmail.com>
2022-04-23 21:53:25 +0900

    Save file
    docx file,
    text file save and load

--- file_creater.py
+++ file_creater.py
@@ -1,5 +1,7 @@
 from tkinter import filedialog
 
+import docx
+
 from Window_set import *    # 기본 윈도우 파일
 import Main_Page as mp      # 메인 페이지 파일
 import Log_in_Page as lip   # 로그인 페이지 파일
@@ -7,11 +9,11 @@ import Log_in_Page as lip   # 로그인 페이지 파일
 import datetime             # 텍스트 현재시간 기록
 import os                   # 파일 디렉토리 접근
 
+from docx import Document   # 워드 저장
+
 
 def delete_fc(next_page):
     memo_txt.place_forget()
-    save_btn.place_forget()
-    open_btn.place_forget()
 
     win_root.config(menu=False)
 
@@ -22,40 +24,72 @@ def delete_fc(next_page):
             mp.Main_Page_set()
 
 
-def save_txt():
-    save_file = open("저장.txt", 'w')
-
-    ctime = os.path.getctime(os.path.abspath(".\\저장.txt"))  # 생성 시간 획득
-    cdate = datetime.datetime.fromtimestamp(ctime).strftime('%Y년 %m월 %d일\n%H시 %M분에 기록 되었습니다.\n\n')
-    save_file.write(cdate)
-
-    save_file.write(memo_txt.get("1.0", END))
-
-
-def open_txt():
-    save_file = open("저장.txt", 'r')
-
-    memo_txt.delete("1.0", END)
-    if os.path.getsize(".\\저장.txt") == 52:          # 기본 날짜만 저장 되었을 경우 크기 == 52
-        memo_txt.insert(END, "저장.txt가 비어있습니다!")
-    else:                                            # 파일에 저장된 내용이 있을 경우 memo에 출력한다.
-        memo_txt.insert(END, save_file.read())
-
-
 memo_txt = Text(win_root, width=115, height=40)
-save_btn = Button(win_root, text="저장하기", command=save_txt)
-open_btn = Button(win_root, text="열어보기", command=open_txt)
+
+
+def save_file():
+    file_name = filedialog.asksaveasfile(parent=win_root, title="파일 선택기",
+                                         defaultextension=".txt",
+                                         filetypes=(("텍스트 파일(.txt)", "*.txt"),
+                                                    ("워드 파일(.docx)", "*.docx"),
+                                                    ("all files", "*.*")
+                                                    )
+                                        )
+
+    if file_name == None:   # 다이얼로그가 강제로 종료되었을 경우(파일이 선택되지 않아 공백임)
+        return
+
+    file_time = os.path.getctime(file_name.name)  # 생성 시간 획득
+    file_time = datetime.datetime.fromtimestamp(file_time).strftime('\n\n%Y년 %m월 %d일 %H시 %M분에 기록 되었습니다.')
+
+    if file_name.name.split('.')[1] == 'docx':
+        doc = Document()
+        doc.add_paragraph(memo_txt.get("1.0", END))
+        doc.add_paragraph(file_time)
+        doc.save(file_name.name)
+
+    elif file_name.name.split('.')[1] == 'txt':
+        save_text = open(file_name.name, 'w')
+        save_text.write(memo_txt.get("1.0", END))
+        save_text.write(file_time)
+        save_text.close()
+
 
 def open_file():
+
     file_name = filedialog.askopenfilename(title='select a text file',
-                                           filetypes=(("text files (.txt)", "*.txt"), ("all files", "*.*")))
+                                           filetypes=(("텍스트 파일(.txt)", "*.txt"),
+                                                      ("워드 파일(.docx)", "*.docx"),
+                                                      ("all files", "*.*")))
+    if file_name == '':  # 다이얼로그가 강제로 종료되었을 경우(파일이 선택되지 않아 공백임)
+        return
+
+    save_file = ''
+    if file_name.split('.')[1] == 'txt':
+        try:
+            save_file = open(file_name, 'r')
+            memo_txt.delete("1.0", END)         # 출력부 미리 비우기
+            memo_txt.insert(END, save_file.read())
+        except UnicodeDecodeError:              # 인코딩 방식을 변경한다.
+            save_file.close()
+            save_file = open(file_name, 'r', encoding='UTF-8')
+            memo_txt.delete("1.0", END)          # 출력부 미리 비우기
+            memo_txt.insert(END, save_file.read())
+        except FileNotFoundError:
+            pass
+    elif file_name.split('.')[1] == 'docx':
+        save_file = docx.Document(file_name)
+
+        memo_txt.delete("1.0", END)  # 출력부 미리 비우기
+        for _, paragraph in enumerate(save_file.paragraphs):
+            memo_txt.insert(END, paragraph.text)
 
 menu = Menu()
 menu_file = Menu(menu, tearoff=False)
-menu_file.add_command(label='Open', command=open_file, accelerator='Ctrl+O')
-menu_file.add_command(label='Save File', accelerator='Ctrl+S', state=DISABLED)
+menu_file.add_command(label='불러오기', command=open_file, accelerator='Ctrl+O')
+menu_file.add_command(label='저장하기', command=save_file, accelerator='Ctrl+S')
 menu_file.add_separator()
-menu_file.add_command(label='Quit', accelerator='Ctrl+Q')
+menu_file.add_command(label='종료', command=lambda: [delete_fc(1)], accelerator='Ctrl+Q')
 
 menu.add_cascade(label='File', menu=menu_file)
 
@@ -68,12 +102,10 @@ class file_creater:
         mp.page_address.delete(0, END)
         mp.page_address.insert(0, f"파일 제작소에 어서오세요! {input_name}님!")
 
-        mp.back_page_btn.configure(command=lambda: [delete_fc(1)])  # 작동 오류: Log-in page의 back_page에서 변경하지 못함
+        mp.back_page_btn.config(command=lambda: [delete_fc(1)])
+        mp.forward_page_btn.config(command=DISABLED)
 
         memo_txt.place(x=100, y=100)
-
-        save_btn.place(x=100, y=70)
-        open_btn.place(x=200, y=70)
 
         win_root.config(menu=menu)
 
